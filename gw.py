4b8ca10f0162985773c96b8aae7d54853b76fcb5
Killian <killiangreene6@gmail.com>
2024-10-16 00:53:38 -0400

    fix ace ranking

--- gw.py
+++ gw.py
@@ -29,7 +29,7 @@ class Card:
 
     def __str__(self):
         symbols = ['\u2664', '\u2661', '\u2667', '\u2662']
-        values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         return f"{symbols[self.suit.value - 1]} {values[self.val - 1]}"
 
     def __eq__(self, other):
